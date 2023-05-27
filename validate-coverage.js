efe4cd6d71094c955c6d71ab911cb534f0ead762
Lucas Couto <lucas3couto@gmail.com>
2023-05-27 18:32:28 -0300

    chore: add coverage validate

---
+++ validate-coverage.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+
+const MINIMUM_COVERAGE = 60 // Defina a cobertura mínima desejada aqui
+
+const lcovFileContent = fs.readFileSync('coverage/lcov.info', 'utf-8')
+const lines = lcovFileContent.split('\n')
+
+let totalLines = 0
+let coveredLines = 0
+
+lines.forEach(line => {
+  if (line.startsWith('DA:')) {
+    totalLines++
+    if (!line.endsWith(',0')) {
+      coveredLines++
+    }
+  }
+})
+
+const coveragePercentage = (coveredLines / totalLines) * 100
+
+if (coveragePercentage < MINIMUM_COVERAGE) {
+  console.error(`Cobertura mínima de teste não atingida. Cobertura atual: ${coveragePercentage.toFixed(2)}%`)
+  process.exit(1) // Sair com código de erro
+} else {
+  console.log(`Cobertura de teste válida. Cobertura atual: ${coveragePercentage.toFixed(2)}%`)
+  process.exit(0) // Sair com sucesso
+}
