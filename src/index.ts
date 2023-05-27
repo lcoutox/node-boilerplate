5c8418a1a78fd86ba6a7a70b38433816ce8c78e5
Lucas Couto <lucas3couto@gmail.com>
2023-05-27 17:35:43 -0300

    feat: add express app

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import './config/module-alias'
+import { env } from '@/config/env'
+import { app } from '@/config/app'
+
+app.listen(env.PORT, () => console.log(`Server running on port: ${env.PORT}`))
